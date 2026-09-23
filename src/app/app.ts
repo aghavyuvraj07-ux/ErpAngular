e56ceb5d6859b7fb3446ac605cd685d0ee6c03dd
Yuvraj Aghav <aghavyuvraj@gmail.com>
2026-09-24 00:03:46 +0530

    Improve ERP dashboard UI with Bootstrap

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { Courses } from './courses/courses';
 import { Students } from './students/students';
 import { Payments } from './payments/payments';
+
+import { Course } from './services/course';
+import { Student } from './services/student';
+import { Payment } from './services/payment';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +13,49 @@ import { Payments } from './payments/payments';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
+
+  courseCount = 0;
+  studentCount = 0;
+  paymentCount = 0;
+
+  constructor(
+    private courseService: Course,
+    private studentService: Student,
+    private paymentService: Payment,
+    private cdr: ChangeDetectorRef
+  ) {}
+
+  ngOnInit(): void {
+
+    this.courseService.getCourses().subscribe({
+      next: (data: any) => {
+        this.courseCount = data.length;
+        this.cdr.detectChanges();
+      },
+      error: (error: any) => {
+        console.error('Course Count Error:', error);
+      }
+    });
+
+    this.studentService.getStudents().subscribe({
+      next: (data: any) => {
+        this.studentCount = data.length;
+        this.cdr.detectChanges();
+      },
+      error: (error: any) => {
+        console.error('Student Count Error:', error);
+      }
+    });
+
+    this.paymentService.getPayments().subscribe({
+      next: (data: any) => {
+        this.paymentCount = data.length;
+        this.cdr.detectChanges();
+      },
+      error: (error: any) => {
+        console.error('Payment Count Error:', error);
+      }
+    });
+  }
 }
